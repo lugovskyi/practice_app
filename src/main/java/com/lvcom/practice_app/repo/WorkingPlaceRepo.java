32cda829cb1c8e261ee8bb82ccb6d8f805485d09
lugovskyi <54972818+lugovskyi@users.noreply.github.com>
2021-06-17 18:07:29 +0300

    SP-47: Fix repository

--- src/main/java/com/lvcom/practice_app/repo/WorkingPlaceRepo.java
+++ src/main/java/com/lvcom/practice_app/repo/WorkingPlaceRepo.java
@@ -1,7 +1,7 @@
 package com.lvcom.practice_app.repo;
 
-import com.lvcom.practice_app.domain.WorkingPlace;
+import com.lvcom.practice_app.domain.Workingplace;
 import org.springframework.data.repository.CrudRepository;
 
-public interface WorkingPlaceRepo extends CrudRepository<WorkingPlace,Long> {
+public interface WorkingPlaceRepo extends CrudRepository<Workingplace,Long> {
 }
