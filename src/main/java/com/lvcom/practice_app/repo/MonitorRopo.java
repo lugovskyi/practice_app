a38b015be39acfbda0cc1c7a98b77d5ad20e55f6
lugovskyi <54972818+lugovskyi@users.noreply.github.com>
2021-06-18 15:48:42 +0300

    SP-47: Add rider dependency

--- src/main/java/com/lvcom/practice_app/repo/MonitorRopo.java
+++
@@ -1,7 +0,0 @@
-package com.lvcom.practice_app.repo;
-
-import com.lvcom.practice_app.domain.Monitor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MonitorRopo extends CrudRepository<Monitor,Long> {
-}
